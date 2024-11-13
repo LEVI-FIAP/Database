a3b172f76766c5356695c0dd0a89ea30562fd463
LuizHNR <luizhneri12@gmail.com>
2024-11-13 19:10:26 -0300

    Revert "atualização"
    This reverts commit 2eafdb3bbe9308a5126380d78c6ac86eef7aaa72.

--- Banco_Projeto/sistema_bd.sql
+++ Banco_Projeto/sistema_bd.sql
@@ -32,6 +32,8 @@ create table Usuario
     email_usu VARCHAR2(100) CONSTRAINT email_usu_nn not null,
     senha_usu VARCHAR2(50) CONSTRAINT senha_usu_nn not null,
     consumo_men NUMBER(8) CONSTRAINT consumo_men_nn not null,
+    custo_energ NUMBER(7) CONSTRAINT cust_ener_nn not null,
+    custo_med NUMBER(7) CONSTRAINT custo_med_nn not null,
     area_desejada NUMBER(7) CONSTRAINT area_des_nn not null,
     id_reg number(2) CONSTRAINT id_reg_fk REFERENCES Regiao,
     id_relatorio number(5) CONSTRAINT id_relatorio_fk REFERENCES Relatorio
@@ -57,13 +59,13 @@ INSERT INTO Relatorio (id_relatorio, qtnd_pain, potencia_total, custo_total, eco
 INSERT INTO Relatorio (id_relatorio, qtnd_pain, potencia_total, custo_total, economia_mensal, payback, energia_mes) VALUES (10, 195, 150, 6000, 320, 16, 1350);
 
 
-INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, area_desejada, id_reg, id_relatorio) VALUES (1, 'Carlos Silva', 'carlos.silva@email.com', 'senha123', 500, 80, 1, 1);
-INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, area_desejada, id_reg, id_relatorio) VALUES (2, 'Ana Pereira', 'ana.pereira@email.com', 'senha456', 800, 120, 2, 2);
-INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, area_desejada, id_reg, id_relatorio) VALUES (3, 'Felipe Costa', 'felipe.costa@email.com', 'senha789', 1000, 150, 3, 3);
-INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, area_desejada, id_reg, id_relatorio) VALUES (4, 'Maria Oliveira', 'maria.oliveira@email.com', 'senha1011', 350, 60, 1, 4);
-INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, area_desejada, id_reg, id_relatorio) VALUES (5, 'José Almeida', 'jose.almeida@email.com', 'senha1213', 600, 90, 2, 5);
-INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, area_desejada, id_reg, id_relatorio) VALUES (6, 'Renata Souza', 'renata.souza@email.com', 'senha1415', 950, 110, 3, 6);
-INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, area_desejada, id_reg, id_relatorio) VALUES (7, 'Lucas Santos', 'lucas.santos@email.com', 'senha1617', 1500, 200, 4, 7);
-INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, area_desejada, id_reg, id_relatorio) VALUES (8, 'Juliana Lima', 'juliana.lima@email.com', 'senha1819', 400, 70, 4, 8);
-INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, area_desejada, id_reg, id_relatorio) VALUES (9, 'Paulo Barbosa', 'paulo.barbosa@email.com', 'senha2021', 750, 130, 2, 9);
-INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, area_desejada, id_reg, id_relatorio) VALUES (10, 'Fernanda Rocha', 'fernanda.rocha@email.com', 'senha2223', 850, 140, 1, 10);
+INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, custo_energ, custo_med, area_desejada, id_reg, id_relatorio) VALUES (1, 'Carlos Silva', 'carlos.silva@email.com', 'senha123', 500, 25, 35, 80, 1, 1);
+INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, custo_energ, custo_med, area_desejada, id_reg, id_relatorio) VALUES (2, 'Ana Pereira', 'ana.pereira@email.com', 'senha456', 800, 30, 45, 120, 2, 2);
+INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, custo_energ, custo_med, area_desejada, id_reg, id_relatorio) VALUES (3, 'Felipe Costa', 'felipe.costa@email.com', 'senha789', 1000, 28, 40, 150, 3, 3);
+INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, custo_energ, custo_med, area_desejada, id_reg, id_relatorio) VALUES (4, 'Maria Oliveira', 'maria.oliveira@email.com', 'senha1011', 350, 20, 30, 60, 1, 4);
+INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, custo_energ, custo_med, area_desejada, id_reg, id_relatorio) VALUES (5, 'José Almeida', 'jose.almeida@email.com', 'senha1213', 600, 27, 33, 90, 2, 5);
+INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, custo_energ, custo_med, area_desejada, id_reg, id_relatorio) VALUES (6, 'Renata Souza', 'renata.souza@email.com', 'senha1415', 950, 32, 38, 110, 3, 6);
+INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, custo_energ, custo_med, area_desejada, id_reg, id_relatorio) VALUES (7, 'Lucas Santos', 'lucas.santos@email.com', 'senha1617', 1500, 35, 50, 200, 4, 7);
+INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, custo_energ, custo_med, area_desejada, id_reg, id_relatorio) VALUES (8, 'Juliana Lima', 'juliana.lima@email.com', 'senha1819', 400, 22, 28, 70, 4, 8);
+INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, custo_energ, custo_med, area_desejada, id_reg, id_relatorio) VALUES (9, 'Paulo Barbosa', 'paulo.barbosa@email.com', 'senha2021', 750, 29, 42, 130, 2, 9);
+INSERT INTO Usuario (id_usu, nome_usu, email_usu, senha_usu, consumo_men, custo_energ, custo_med, area_desejada, id_reg, id_relatorio) VALUES (10, 'Fernanda Rocha', 'fernanda.rocha@email.com', 'senha2223', 850, 33, 47, 140, 1, 10);
