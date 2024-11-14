b0fc4770faf8d07a23d3c38ba127b3e83c3259fb
LuizHNR <luizhneri12@gmail.com>
2024-11-14 13:53:16 -0300

    OPS

--- Banco_Projeto/sistema_bd.sql
+++ Banco_Projeto/sistema_bd.sql
@@ -33,6 +33,7 @@ CREATE table Relatorio
     custo_instal NUMBER(12,2) CONSTRAINT custo_isntal_nn not null,
     economia_mensal NUMBER(12,2) CONSTRAINT econ_men_nn not null,
     payback NUMBER(12,2) CONSTRAINT payback_nn not null,
+    energia_mes NUMBER(10) CONSTRAINT energia_mes_nn not null,
     id_usu NUMBER(5) CONSTRAINT id_usu_fk REFERENCES Usuario,
     id_reg number(2) CONSTRAINT id_reg_fk REFERENCES Regiao
 );
@@ -56,13 +57,13 @@ INSERT INTO Usuario (id_usu, nome_usuario, email, senha) VALUES (8, 'Patricia Ma
 INSERT INTO Usuario (id_usu, nome_usuario, email, senha) VALUES (9, 'Lucas Santos', 'lucas.santos@email.com', 'senha333');
 INSERT INTO Usuario (id_usu, nome_usuario, email, senha) VALUES (10, 'Gabriela Rocha', 'gabriela.rocha@email.com', 'senha444');
 
-INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, id_usu, id_reg) VALUES (1, 350.50, 120.75, 45, 15, 5.4, 25000.00, 500.00, 4.2, 1, 1);
-INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, id_usu, id_reg) VALUES (2, 500.25, 180.50, 60, 18, 6.0, 28000.00, 750.00, 3.5, 2, 2);
-INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, id_usu, id_reg) VALUES (3, 620.00, 250.00, 75, 20, 6.5, 30000.00, 900.00, 3.0, 3, 3);
-INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, id_usu, id_reg) VALUES (4, 280.75, 100.30, 40, 12, 4.8, 22000.00, 450.00, 4.8, 4, 1);
-INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, id_usu, id_reg) VALUES (5, 460.00, 190.00, 65, 17, 5.9, 27000.00, 700.00, 3.7, 5, 2);
-INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, id_usu, id_reg) VALUES (6, 540.00, 220.00, 70, 19, 6.3, 29000.00, 800.00, 3.3, 6, 3);
-INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, id_usu, id_reg) VALUES (7, 380.25, 140.50, 50, 14, 5.0, 24000.00, 600.00, 4.5, 7, 1);
-INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, id_usu, id_reg) VALUES (8, 400.75, 150.00, 55, 16, 5.2, 26000.00, 650.00, 4.0, 8, 2);
-INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, id_usu, id_reg) VALUES (9, 650.50, 260.00, 80, 22, 6.8, 32000.00, 950.00, 2.9, 9, 3);
-INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, id_usu, id_reg) VALUES (10, 310.00, 130.00, 42, 13, 5.1, 23000.00, 530.00, 4.3, 10, 1);
+INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, energia_mes, id_usu, id_reg) VALUES (1, 350.50, 120.75, 45, 15, 5.4, 25000.00, 500.00, 4.2, 150, 1, 1);
+INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, energia_mes, id_usu, id_reg) VALUES (2, 500.25, 180.50, 60, 18, 6.0, 28000.00, 750.00, 3.5, 160, 2, 2);
+INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, energia_mes, id_usu, id_reg) VALUES (3, 620.00, 250.00, 75, 20, 6.5, 30000.00, 900.00, 3.0, 170, 3, 3);
+INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, energia_mes, id_usu, id_reg) VALUES (4, 280.75, 100.30, 40, 12, 4.8, 22000.00, 450.00, 4.8, 180, 4, 1);
+INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, energia_mes, id_usu, id_reg) VALUES (5, 460.00, 190.00, 65, 17, 5.9, 27000.00, 700.00, 3.7, 190, 5, 2);
+INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, energia_mes, id_usu, id_reg) VALUES (6, 540.00, 220.00, 70, 19, 6.3, 29000.00, 800.00, 3.3, 110, 6, 3);
+INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, energia_mes, id_usu, id_reg) VALUES (7, 380.25, 140.50, 50, 14, 5.0, 24000.00, 600.00, 4.5, 120, 7, 1);
+INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, energia_mes, id_usu, id_reg) VALUES (8, 400.75, 150.00, 55, 16, 5.2, 26000.00, 650.00, 4.0, 130, 8, 2);
+INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, energia_mes, id_usu, id_reg) VALUES (9, 650.50, 260.00, 80, 22, 6.8, 32000.00, 950.00, 2.9, 140, 9, 3);
+INSERT INTO Relatorio (id_relatorio, consumo_mensal, conta_luz, area_desejada, qtd_paineis, potencia_total, custo_instal, economia_mensal, payback, energia_mes, id_usu, id_reg) VALUES (10, 310.00, 130.00, 42, 13, 5.1, 23000.00, 530.00, 4.3, 150, 10, 1);
